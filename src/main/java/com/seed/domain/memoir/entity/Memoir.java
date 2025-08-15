e78caf9bf8b77f30c7cd49fbaa486dc2a82a1d83
BaekDoHyeon <qorehgus0823@naver.com>
2025-08-15 19:46:42 +0900

    feat : Enum 및 Entity 클래스 수정 작업     - EnumCode 에서 null 처리 로직 추가     - BaseEntity 에서 LocalDateTime 값 넣을때 초 단위 까지만 저장     - User 클래스에서 userId 값만 가지는 객체 반환 정적 메서드 추가

--- src/main/java/com/seed/domain/memoir/entity/Memoir.java
+++ src/main/java/com/seed/domain/memoir/entity/Memoir.java
@@ -1,7 +1,9 @@
 package com.seed.domain.memoir.entity;
 
+import com.seed.domain.attachment.entity.Attachment;
 import com.seed.domain.memoir.enums.*;
-import com.seed.domain.schedule.entity.Schedule;
+import com.seed.domain.schedule.enums.InterviewStep;
+import com.seed.domain.schedule.enums.Position;
 import com.seed.domain.user.entity.User;
 import com.seed.global.entity.BaseEntity;
 import jakarta.persistence.*;
@@ -9,6 +11,7 @@ import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,9 +28,8 @@ public class Memoir extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "schedule_id")
-    private Schedule schedule;
+    @OneToMany(mappedBy = "memoir", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<Attachment> attachments;
 
     @Column(length = 2)
     @Convert(converter = MemoirType.JpaConverter.class)
@@ -64,18 +66,28 @@ public class Memoir extends BaseEntity {
     @Comment("면접방식")
     private InterviewMethod interviewMethod;
 
-    @Comment("면접진행시간")
-    private int interviewDuration;
-
     @Column(length = 500)
     private String freeNote;
-
-    private String interviewPlace;
 
     @Column(length = 500)
     private String url;
 
-    private int helpCount;
+    private String companyName;
+
+    @Column(length = 2)
+    @Convert(converter = Position.JpaConverter.class)
+    @Comment("직무")
+    private Position position;
+
+    @Column(length = 2)
+    @Convert(converter = InterviewStep.JpaConverter.class)
+    @Comment("면접단계")
+    private InterviewStep interviewStep;
+
+    @Column(columnDefinition = "DATETIME(0)")
+    private LocalDateTime interviewTime;
+
+    private int likeCount;
 
     private int viewCount;
 
