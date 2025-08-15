a8d2495e21315123d2826ba93cb9515593d52e63
BaekDoHyeon <qorehgus0823@naver.com>
2025-08-15 19:57:35 +0900

    feat (memoir) : 퀵 회고 등록 API 구현     - 퀵회고 요청 DTO 생성

--- src/main/java/com/seed/domain/memoir/service/MemoirService.java
+++ src/main/java/com/seed/domain/memoir/service/MemoirService.java
@@ -1,4 +1,7 @@
 package com.seed.domain.memoir.service;
 
+import com.seed.domain.memoir.dto.request.QuickMemoirRequest;
+
 public interface MemoirService {
+    Long createQuickMemoir(QuickMemoirRequest quickMemoirRequest);
 }
--- src/main/java/com/seed/domain/memoir/service/impl/MemoirServiceImpl.java
+++ src/main/java/com/seed/domain/memoir/service/impl/MemoirServiceImpl.java
@@ -1,5 +1,7 @@
 package com.seed.domain.memoir.service.impl;
 
+import com.seed.domain.memoir.dto.request.QuickMemoirRequest;
+import com.seed.domain.memoir.entity.Memoir;
 import com.seed.domain.memoir.repository.MemoirRepository;
 import com.seed.domain.memoir.service.MemoirService;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class MemoirServiceImpl implements MemoirService {
 
     private final MemoirRepository memoirRepository;
+
+    @Override
+    public Long createQuickMemoir(QuickMemoirRequest quickMemoirRequest) {
+        Memoir memoir = QuickMemoirRequest.toEntity(quickMemoirRequest);
+        Memoir save = memoirRepository.save(memoir);
+        return save.getId();
+    }
 }
