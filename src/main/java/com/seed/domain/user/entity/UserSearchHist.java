dd85758a0a4b7c341d8c9fb0b1f41b850a23dd68
BaekDoHyeon <qorehgus0823@naver.com>
2025-08-19 20:40:37 +0900

    feat (UserSearchHist) : 유저 검색 기록 엔티티 클래스 추가

---
+++ src/main/java/com/seed/domain/user/entity/UserSearchHist.java
@@ -0,0 +1,23 @@
+package com.seed.domain.user.entity;
+
+import com.seed.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class UserSearchHist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+}
