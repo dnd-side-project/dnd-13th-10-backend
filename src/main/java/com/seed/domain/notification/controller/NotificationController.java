6ae77279c2ae4f84ee5ecdeb018080d8e8f75e04
BaekDoHyeon <qorehgus0823@naver.com>
2025-08-27 23:56:47 +0900

    refactor (swagger) : 알림(Notification) 스웨거 작성 수정 및 시스템 알림 생성 테스트 API 에서 입력 값 세팅 안되는 이슈 해결

--- src/main/java/com/seed/domain/notification/controller/NotificationController.java
+++ src/main/java/com/seed/domain/notification/controller/NotificationController.java
@@ -1,6 +1,5 @@
 package com.seed.domain.notification.controller;
 
-import com.seed.domain.notification.dto.reqeust.NotificationCreateRequest;
 import com.seed.domain.notification.dto.response.NotificationResponse;
 import com.seed.domain.notification.service.NotificationService;
 import com.seed.domain.user.entity.User;
@@ -9,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,13 +32,17 @@ public class NotificationController {
 
     // 알림 생성 테스트용
     @Operation(
-            summary = "[테스트용] 알림을 생성하는 ",
-            description = "면접 회고 리스트를 조회하는 API 입니다.<br>"
-                    + "- 퀵/일반 회고 모두 조회합니다. (임시저장은 제외)"
+            summary = "[테스트용] 회고 알림을 생성하는 API",
+            description = "시스템 알림(회고 알림)을 생성하는 테스트용 API 입니다.<br>"
+                    + "- 실제로는 자동으로 스케줄을 통해 면접일정이 7일이 지난 건에 대한 알림이 자동으로 생성됩니다.<br>"
+                    + "- 이외의 커뮤니티 알림 생성하는 방법은 누군가 나의 회고에 댓글을 달면 알림이 생성됩니다."
     )
     @PostMapping
-    public ApiResponse<?> createNotification(@AuthenticationPrincipal User user, NotificationCreateRequest createRequest) {
-        notificationService.createNotification(user.getId(), createRequest.getContent());
+    public ApiResponse<?> createNotification(
+            @AuthenticationPrincipal User user,
+            @RequestParam String content
+            ) {
+        notificationService.createNotification(user.getId(), content);
         return ApiResponse.success();
     }
 
